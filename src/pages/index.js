321024069dffa25f850a50b8d6b3b9d0d165213c
Gabby Pelton <gabriellapelton@gmail.com>
2021-04-04 16:32:43 -0500

    SEO

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,10 @@ import get from "lodash/get";
 import { Helmet } from "react-helmet";
 import Hero from "../components/hero";
 import Layout from "../components/layout";
-import ArticlePreview from "../components/article-preview";
 
 class RootIndex extends React.Component {
   render() {
     const siteTitle = get(this, "props.data.site.siteMetadata.title");
-    const posts = get(this, "props.data.allContentfulBlogPost.edges");
     const [author] = get(this, "props.data.allContentfulPerson.edges");
 
     return (
